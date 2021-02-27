4f6d0adf06c695db815408fda46d50c77a00bcac
Lucas <lxavier.00@outlook.com>
2021-02-27 14:01:10 -0300

    App Movi.it - state dos dados funcionando

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,7 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
-
+import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
-
 interface Challenge {
   type: 'body' | 'eye';
   description: string;
@@ -22,14 +21,22 @@ interface ChallengesContextData {
 
 interface ChallengesProviderProps {
   children: ReactNode
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
+
 
 export const ChallengesContext = createContext({} as ChallengesContextData)
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-  const [level, setLevel] = useState(1)
-  const [currentExperience, setCurrentExperience] = useState(0)
-  const [challengesCompleted, setChallengesCompleted] = useState(0)
+export function ChallengesProvider({ 
+  children, 
+  ...rest
+}: ChallengesProviderProps) {
+
+  const [level, setLevel] = useState(rest.level ?? 1)
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0)
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0)
   
   const [activeChallenge, setActiveChallenge] = useState(null)
 
@@ -38,6 +45,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   useEffect(() => {
     Notification.requestPermission()
   }, [])
+
+  useEffect(() => {
+    Cookies.set('level', String(level));
+    Cookies.set('currentExperience', String(currentExperience));
+    Cookies.set('challengesCompleted', String(challengesCompleted));
+  }, [level, currentExperience, challengesCompleted])
 
   function levelUp() {
     setLevel(level + 1)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import '../styles/global.css'
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
-
 function MyApp({ Component, pageProps }) {
   
 
   return (
-    <ChallengesProvider>
+    
         <Component {...pageProps} />
-    </ChallengesProvider>
+    
   )
 }
 
