eb148cda0af610b1369958ad7c2e543bd86c9573
Lucas <lxavier.00@outlook.com>
2021-02-27 17:17:39 -0300

    App Movi.it - criada e add a funcionalidade do modal

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
+import { LevelUpModal } from '../components/LevelUpModal'
 interface Challenge {
   type: 'body' | 'eye';
   description: string;
@@ -17,6 +18,7 @@ interface ChallengesContextData {
   startNewChallenge: () => void;
   resetChallenge: () => void;
   completeChallenge: () => void;
+  closeLevelUpModal: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -39,6 +41,7 @@ export function ChallengesProvider({
   const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0)
   
   const [activeChallenge, setActiveChallenge] = useState(null)
+  const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false)
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
@@ -54,7 +57,13 @@ export function ChallengesProvider({
 
   function levelUp() {
     setLevel(level + 1)
+    setIsLevelUpModalOpen(true)
   }
+
+  function closeLevelUpModal() {
+    setIsLevelUpModalOpen(false)
+  }
+
 
   function startNewChallenge() {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
@@ -102,10 +111,13 @@ export function ChallengesProvider({
       levelUp,
       startNewChallenge,
       resetChallenge,
-      completeChallenge
+      completeChallenge,
+      closeLevelUpModal
       }}
     >
       { children }
+
+    {isLevelUpModalOpen &&  <LevelUpModal/>}
     </ChallengesContext.Provider>
   )
 
